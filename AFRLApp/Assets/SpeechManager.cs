9ba30563a255edc13257386a9b17f2227950ede2
Daniel Eckley <Daniel Eckley>
2017-02-02 16:56:57 -0500

    Created the event handlers for the various voice commands

--- AFRLApp/Assets/SpeechManager.cs
+++ AFRLApp/Assets/SpeechManager.cs
@@ -12,19 +12,76 @@ public class SpeechManager : MonoBehaviour
     // Use this for initialization
     void Start()
     {
-        keywords.Add("Reset world", () =>
+        keywords.Add("Open Gallery", () =>
         {
             // Call the OnReset method on every descendant object.
-            this.BroadcastMessage("OnReset");
+
+            // Add correct function here, not OnGallerySelect
+            this.BroadcastMessage("OnGallerySelect");
         });
 
-        keywords.Add("Run Script", () =>
+        keywords.Add("Close Gallery", () =>
+        {
+            // Call the OnReset method on every descendant object.
+
+            // Add correct function here, not OnGallerySelect
+            this.BroadcastMessage("OnGallerySelect");
+        });
+
+        // This is redundant with the select command, just here for testing; remove 
+        // once testing is finished
+
+        keywords.Add("Swap Image", () =>
         {
             var focusObject = GazeManager.Instance.FocusedObject;
             if (focusObject != null)
             {
                 // Call the OnDrop method on just the focused object.
                 focusObject.SendMessage("OnSelect");
+            }
+        });
+
+        keywords.Add("Next Image", () =>
+        {
+            var focusObject = GazeManager.Instance.FocusedObject;
+            if (focusObject != null)
+            {
+                // Add actual OnNextImage function
+
+                focusObject.SendMessage("OnNextImage");
+            }
+        });
+
+        keywords.Add("Previous Image", () =>
+        {
+            var focusObject = GazeManager.Instance.FocusedObject;
+            if (focusObject != null)
+            {
+                // Add actual OnNextImage function
+
+                focusObject.SendMessage("OnPreviousImage");
+            }
+        });
+
+        keywords.Add("Follow Me", () =>
+        {
+            var focusObject = GazeManager.Instance.FocusedObject;
+            if (focusObject != null)
+            {
+                // Add actual OnFollow function
+                
+                this.BroadcastMessage("OnFollow");
+            }
+        });
+
+        keywords.Add("Stop Following", () =>
+        {
+            var focusObject = GazeManager.Instance.FocusedObject;
+            if (focusObject != null)
+            {
+                // Add OnStopFollowing function
+                
+                this.BroadcastMessage("OnStopFollowing");
             }
         });
 
