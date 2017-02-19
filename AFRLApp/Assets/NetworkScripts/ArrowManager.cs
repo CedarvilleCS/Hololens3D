6e3f2accdf6b34ec3af65d4c0fd883c0221ed7fc
Nathan Harold <nharold@cedarville.edu>
2017-02-19 16:24:45 -0500

    used HoloToolkit's spatial mapping integration to stick the spheres to the wall

--- AFRLApp/Assets/NetworkScripts/ArrowManager.cs
+++ AFRLApp/Assets/NetworkScripts/ArrowManager.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using HoloToolkit.Unity;
 
 public class ArrowManager : MonoBehaviour {
 
@@ -11,11 +12,32 @@ public class ArrowManager : MonoBehaviour {
         _objr.PositionIDRequestReceived += OnPositionIDRequestReceived;
 
         _imagePositions = new System.Collections.Generic.Dictionary<uint, HLNetwork.ImagePosition>();
+        spatialMappingManager = SpatialMappingManager.Instance;
     }
 
     public Transform arrowPrefab;
-	void Update () {
+    private SpatialMappingManager spatialMappingManager;
+    void Update () {
         Instantiate(arrowPrefab, Camera.main.transform.position, Quaternion.identity);
+
+        // Code largely thanks to HoloToolkit/SpatialMapping/Scripts/TapToPlace.cs
+        var headPosition = Camera.main.transform.position;
+        var gazeDirection = Camera.main.transform.forward;
+
+        RaycastHit hitInfo;
+        if (Physics.Raycast(headPosition, gazeDirection, out hitInfo,
+            30.0f, spatialMappingManager.LayerMask))
+        {
+            Quaternion toQuat = Camera.main.transform.localRotation;
+            Instantiate(arrowPrefab, hitInfo.point, toQuat);
+        }
+        else
+        {
+            Vector3 pos = Camera.main.transform.forward;
+            pos.Scale(new Vector3(3.0f, 3.0f, 3.0f));
+            pos += Camera.main.transform.position;
+            Instantiate(arrowPrefab, pos, Quaternion.identity);
+        }
     }
 
     void OnPositionIDRequestReceived(object obj, HLNetwork.PositionIDRequestReceivedEventArgs args)
