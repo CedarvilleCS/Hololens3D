f354271751469502faaee7294a3076307ae5a753
Nathan Harold <nharold@cedarville.edu>
2017-02-22 14:33:41 -0500

    removed sphere generation test and room mesh display

--- AFRLApp/Assets/NetworkScripts/ArrowManager.cs
+++ AFRLApp/Assets/NetworkScripts/ArrowManager.cs
@@ -18,26 +18,26 @@ public class ArrowManager : MonoBehaviour {
     public Transform arrowPrefab;
     private SpatialMappingManager spatialMappingManager;
     void Update () {
-        Instantiate(arrowPrefab, Camera.main.transform.position, Quaternion.identity);
+        //Instantiate(arrowPrefab, Camera.main.transform.position, Quaternion.identity);
 
-        // Code largely thanks to HoloToolkit/SpatialMapping/Scripts/TapToPlace.cs
-        var headPosition = Camera.main.transform.position;
-        var gazeDirection = Camera.main.transform.forward;
+        //// Code largely thanks to HoloToolkit/SpatialMapping/Scripts/TapToPlace.cs
+        //var headPosition = Camera.main.transform.position;
+        //var gazeDirection = Camera.main.transform.forward;
 
-        RaycastHit hitInfo;
-        if (Physics.Raycast(headPosition, gazeDirection, out hitInfo,
-            30.0f, spatialMappingManager.LayerMask))
-        {
-            Quaternion toQuat = Camera.main.transform.localRotation;
-            Instantiate(arrowPrefab, hitInfo.point, toQuat);
-        }
-        else
-        {
-            Vector3 pos = Camera.main.transform.forward;
-            pos.Scale(new Vector3(3.0f, 3.0f, 3.0f));
-            pos += Camera.main.transform.position;
-            Instantiate(arrowPrefab, pos, Quaternion.identity);
-        }
+        //RaycastHit hitInfo;
+        //if (Physics.Raycast(headPosition, gazeDirection, out hitInfo,
+        //    30.0f, spatialMappingManager.LayerMask))
+        //{
+        //    Quaternion toQuat = Camera.main.transform.localRotation;
+        //    Instantiate(arrowPrefab, hitInfo.point, toQuat);
+        //}
+        //else
+        //{
+        //    Vector3 pos = Camera.main.transform.forward;
+        //    pos.Scale(new Vector3(3.0f, 3.0f, 3.0f));
+        //    pos += Camera.main.transform.position;
+        //    Instantiate(arrowPrefab, pos, Quaternion.identity);
+        //}
     }
 
     void OnPositionIDRequestReceived(object obj, HLNetwork.PositionIDRequestReceivedEventArgs args)
