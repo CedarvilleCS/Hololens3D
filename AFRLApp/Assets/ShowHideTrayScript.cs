42b0b1b1f205140e4ebdfc0278ced2339384628b
Jesse Russell <herewegojes1@gmail.com>
2017-09-18 13:09:07 -0400

    Added canvas and beginnings of HUD

---
+++ AFRLApp/Assets/ShowHideTrayScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShowHideTrayScript : MonoBehaviour {
+    private GameObject TrayAddButton; 
+
+
+	// Use this for initialization
+	void Start () {
+        TrayAddButton = GameObject.Find("TrayAddButton");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void showTray()
+    {
+        TrayAddButton.SetActive(true);
+        this.GetComponent<Material>().SetTexture("HideTrayMaterial", (Texture2D)Resources.Load("004-hidetray.png"));
+    }
+
+    public void hideTray()
+    {
+        TrayAddButton.SetActive(false);
+        this.GetComponent<Material>().SetTexture("HideTrayMaterial", (Texture2D)Resources.Load("005-showtray.png"));
+    }
+}
