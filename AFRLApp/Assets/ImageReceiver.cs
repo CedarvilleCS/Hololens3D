31366d5cb9dc6eaee8db3f4d109f3f08931d181a
tfroberg <tfroberg@cedarville.edu>
2017-09-23 16:01:53 -0400

    Add second object receiver for test images

--- AFRLApp/Assets/ImageReceiver.cs
+++ AFRLApp/Assets/ImageReceiver.cs
@@ -1,19 +1,23 @@
 ﻿using UnityEngine;
-using System.Collections;
 
 public class ImageReceiver : MonoBehaviour
 {
     private byte[] _nextImageData;
     private bool _newImagePresent;
+    private bool _newTestImagePresent;
     public bool FirstInstance = true;
     public int NumRcvdImages = 0;
-    public int ResetNumRcvdImages;
+    public int ResetNumRcvdImages; 
 
     void Start()
     {
         HLNetwork.ObjectReceiver objr = HLNetwork.ObjectReceiver.getTheInstance();
+        HLNetwork.ObjectReceiver testReceiver = HLNetwork.ObjectReceiver.getTestInstance();
+
         objr.JpegReceived += OnJpegReceived;
-        if(!FirstInstance)
+        testReceiver.JpegReceived += TestJpegReceived;
+
+        if (!FirstInstance)
         {
             NumRcvdImages = ResetNumRcvdImages;
         }
@@ -22,27 +26,33 @@ public class ImageReceiver : MonoBehaviour
 
     void Update()
     {
-        if (_newImagePresent)
+        if (_newImagePresent || _newTestImagePresent)
         {
             NumRcvdImages++;
             Texture2D tex = new Texture2D(2, 2);
             tex.LoadImage(_nextImageData);
-            
-            GameObject ImageGallery = this.transform.Find("ImageGallery").gameObject;
-            GameObject ImageQueue = this.transform.Find("ImageQueue").gameObject;
+
+            GameObject ImageGallery = transform.Find("ImageGallery").gameObject;
+            GameObject ImageQueue = transform.Find("ImageQueue").gameObject;
             ImageGallery.GetComponent<ImageGalleryController>().RcvNewImage(tex, NumRcvdImages);
             ImageQueue.GetComponent<ImageQueueController>().RcvNewImage(tex, NumRcvdImages);
 
             // Only load received image into main image pane if it is the first image received
 
-            if (NumRcvdImages == 1)
+            if (NumRcvdImages == 1 && !_newTestImagePresent)
             {
-                GameObject AnnotatedImage = this.transform.Find("AnnotatedImage").gameObject;
+                GameObject AnnotatedImage = transform.Find("AnnotatedImage").gameObject;
                 AnnotatedImage.GetComponent<AnnotatedImageController>().DisplayImage(tex);
             }
-            
-            _newImagePresent = false;
+
+            _newImagePresent = _newTestImagePresent = false;
         }
+    }
+
+    void TestJpegReceived(object obj, HLNetwork.JpegReceivedEventArgs args)
+    {
+        _nextImageData = args.Image;
+        _newTestImagePresent = true;
     }
 
     void OnJpegReceived(object obj, HLNetwork.JpegReceivedEventArgs args)
@@ -54,6 +64,9 @@ public class ImageReceiver : MonoBehaviour
     public void OnWindowClosed()
     {
         HLNetwork.ObjectReceiver objr = HLNetwork.ObjectReceiver.getTheInstance();
+        HLNetwork.ObjectReceiver testReceiver = HLNetwork.ObjectReceiver.getTestInstance();
+
         objr.JpegReceived -= OnJpegReceived;
+        testReceiver.JpegReceived -= TestJpegReceived;
     }
 }
