faf79807df1cb822e073d767b6530fd1239da6d5
Jesse Russell <jrussell107@cedarville.edu>
2017-10-12 14:00:11 -0400

    Finishing up the new PDF gallery, prepping to recieve scripts

--- AFRLApp/Assets/PDFGalleryController.cs
+++ AFRLApp/Assets/PDFGalleryController.cs
@@ -2,11 +2,11 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PDFReciever : MonoBehaviour {
+public class PDFGalleryController : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-
+        this.enabled = false;
 	}
 	
 	// Update is called once per frame
