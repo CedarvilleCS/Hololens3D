179818bf89dd93525499cd707674bea789224e4f
tylerbowdish <tylerbowdish@cedarville.edu>
2017-10-17 14:51:43 -0400

    Added backend PDF support

--- AFRLApp/Assets/DataManager.cs
+++ AFRLApp/Assets/DataManager.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class DataManager : MonoBehaviour {
-    List<PDFDocument> documents;
+    public List<PDFDocument> documents;
 
 	// Use this for initialization
 	void Start () {
