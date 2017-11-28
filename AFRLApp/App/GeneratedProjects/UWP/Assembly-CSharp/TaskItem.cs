03875e4a64fb71686d70ed6aa693112d389b37d1
tylerbowdish <tylerbowdish@cedarville.edu>
2017-11-28 16:16:20 -0500

    Added TaskItem, TaskList classes, added receiver to HLNetwork.ObjectReceiver

---
+++ AFRLApp/App/GeneratedProjects/UWP/Assembly-CSharp/TaskItem.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+
+public class TaskItem
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public bool IsCompleted { get; set; }
+    public byte[] Attachment { get; set; }
+
+    public TaskItem()
+    {
+        IsCompleted = false;
+    }
+
+    public TaskItem(int id, string name)
+    {
+        Id = id;
+        Name = Name;
+        IsCompleted = false;
+    }
+
+    public static TaskItem FromByteArray(byte[] bytes)
+    {
+        var id = BitConverter.ToInt32(SubArray(bytes, 0, 4), 0);
+        var isCompleted = BitConverter.ToBoolean(SubArray(bytes, 4, 1), 0);
+
+        var nameLength = BitConverter.ToInt32(SubArray(bytes, 5, 4), 0);
+        var currentPosition = 9 + nameLength;
+        var name = Encoding.ASCII.GetString(SubArray(bytes, 9, nameLength));
+
+        var imageLength = BitConverter.ToInt32(SubArray(bytes, currentPosition, 4), 0);
+        var imageBytes = SubArray(bytes, currentPosition + 4, imageLength);
+
+        return new TaskItem
+        {
+            Id = id,
+            Name = name,
+            IsCompleted = isCompleted,
+            Attachment = imageBytes
+        };
+    }
+
+    public byte[] ToByteArray()
+    {
+        var idBytes = BitConverter.GetBytes(Id);
+
+        var nameBytes = Encoding.ASCII.GetBytes(Name);
+        var nameBytesLength = BitConverter.GetBytes(nameBytes.Length);
+
+        var completedBytes = BitConverter.GetBytes(IsCompleted);
+        var imageBytesLength = BitConverter.GetBytes(Attachment.Length);
+
+        var allBytes = idBytes.Concat(completedBytes)
+            .Concat(nameBytesLength)
+            .Concat(nameBytes)
+            .Concat(imageBytesLength)
+            .Concat(Attachment)
+            .ToArray();
+
+        return (BitConverter.GetBytes(allBytes.Length + 4)).Concat(allBytes).ToArray();
+    }
+
+    public static byte[] SubArray(byte[] data, int start, int length)
+    {
+        byte[] toReturn = new byte[length];
+        Array.Copy(data, start, toReturn, 0, length);
+        return toReturn;
+    }
+}
---
+++ AFRLApp/App/GeneratedProjects/UWP/Assembly-CSharp/TaskListReceiver.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssemblyCSharpWSA
+{
+    class TaskListReceiver : MonoBehaviour
+    {
+        private TaskList _nextTaskList;
+        private bool _newTaskListPresent;
+        public bool FirstInstance = true;
+        public int NumRcvdTaskLists = 0;
+        public int ResetNumRcvdTaskLists;
+        public List<TaskList> taskLists;
+
+        private void Awake()
+        {
+            taskLists = new List<TaskList>();
+            HLNetwork.ObjectReceiver objr = HLNetwork.ObjectReceiver.getTheInstance();
+            objr.TaskListReceived += OnTaskListReceived;
+            if (!FirstInstance)
+            {
+                NumRcvdTaskLists = ResetNumRcvdTaskLists;
+            }
+        }
+
+        private void Update()
+        {
+            if (_newTaskListPresent)
+            {
+                taskLists.Add(_nextTaskList);
+                _newTaskListPresent = false;
+                Debug.Log("Received new Task List");
+                NumRcvdTaskLists++;
+
+                //TODO: send task list to GUI
+            }
+        }
+
+        void OnTaskListReceived(object obj, HLNetwork.TaskListReceivedEventArgs args)
+        {
+            _nextTaskList = args.TaskList;
+            _newTaskListPresent = true;
+        }
+
+        public void OnWindowClosed()
+        {
+            HLNetwork.ObjectReceiver objr = HLNetwork.ObjectReceiver.getTheInstance();
+            objr.TaskListReceived -= OnTaskListReceived;
+        }
+
+        public int GetNumTaskLists()
+        {
+            return taskLists.Count;
+        }
+    }
+}
