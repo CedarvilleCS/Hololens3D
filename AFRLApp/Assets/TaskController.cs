89db6aca78a83b3489e191a8479f26f125ae1c26
Jesse Russell <herewegojes1@gmail.com>
2018-01-15 14:06:11 -0500

    Working on the tasklist still

--- AFRLApp/Assets/TaskController.cs
+++ AFRLApp/Assets/TaskController.cs
@@ -1,11 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TaskController : MonoBehaviour {
 
     public GameObject checkButton;
     public GameObject showImageButton;
+    public Text TaskText;
 	// Use this for initialization
 	void Start () {
 		
